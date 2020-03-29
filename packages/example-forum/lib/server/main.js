d0c0a0a279124f235703b0a15461baf73216375a
yt <yt@arctop.co>
2020-03-29 13:34:50 +0300

    change settings to allow seeding

--- packages/example-forum/lib/server/main.js
+++ packages/example-forum/lib/server/main.js
@@ -5,9 +5,9 @@ export * from '../modules/index.js';
 
 // Server
 import './emails/index.js';
-import './seed/index.js';
 import './comments/index.js';
 import './posts/index.js';
 import './settings.js';
+import './seed/index.js';
 import './rss.js';
 import './api.js';
