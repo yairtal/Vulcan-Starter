95ca79538187d68659eeb38f2ae17340bc57ce83
yt <yt@arctop.co>
2020-03-29 14:23:42 +0300

    add seed of pics

--- packages/example-forum/lib/server/seed/seed_posts.js
+++ packages/example-forum/lib/server/seed/seed_posts.js
@@ -5,6 +5,7 @@ import Users from 'meteor/vulcan:users';
 import { Promise } from 'meteor/promise';
 import { Posts } from '../../modules/posts/index.js';
 import { Comments } from '../../modules/comments/index.js';
+import Pics from '../../modules/pics/collection.js';
 
 if (getSetting('forum.seedOnStart')) {
 
@@ -20,6 +21,7 @@ if (getSetting('forum.seedOnStart')) {
   Users.addField(dummyFlag);
   Posts.addField(dummyFlag);
   Comments.addField(dummyFlag);
+  Pics.addField(dummyFlag);
 
   Posts.addField({
     fieldName: 'dummySlug',
@@ -31,26 +33,45 @@ if (getSetting('forum.seedOnStart')) {
   });
 
   const toTitleCase = (str) => {
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    return str.replace(/\w\S*/g, function(txt) {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  };
+
+  const createPic = async (imageUrl, createdAt, body, username) => {
+    const currentUser = await Users.rawCollection().findOne({username: username});
+    const pic = {
+      createdAt,
+      imageUrl: `http://vulcanjs.org/photos/${imageUrl}`,
+      body,
+      isDummy: true,
+      userId: currentUser._id,
+    };
+
+    return newMutation({
+      collection: Pics,
+      document: pic,
+      currentUser: currentUser,
+      validate: false,
+    });
   };
 
   const createPost = async (slug, postedAt, username, thumbnail) => {
-    const currentUser = await Users.rawCollection().findOne({ username: username });
+    const currentUser = await Users.rawCollection().findOne({username: username});
     const document = {
       postedAt,
-      body: Assets.getText("lib/assets/content/" + slug + ".md"),
+      picId: _.sample(await Pics.rawCollection().find({}).toArray())._id,
+      body: Assets.getText('lib/assets/content/' + slug + '.md'),
       title: toTitleCase(slug.replace(/_/g, ' ')),
       dummySlug: slug,
       isDummy: true,
       userId: currentUser._id,
     };
 
-    if (typeof thumbnail !== "undefined") {
-      document.thumbnailUrl = "/packages/example-forum/lib/assets/images/" + thumbnail;
+    if (typeof thumbnail !== 'undefined') {
+      document.thumbnailUrl = '/packages/example-forum/lib/assets/images/' + thumbnail;
     }
 
     return createMutator({
-      collection: Posts, 
+      collection: Posts,
       document,
       currentUser,
       validate: false,
@@ -58,8 +79,8 @@ if (getSetting('forum.seedOnStart')) {
   };
 
   const createComment = async (slug, username, body, parentBody) => {
-    const user = await Users.rawCollection().findOne({ username: username });
-    const post = await Posts.rawCollection().findOne({ dummySlug: slug });
+    const user = await Users.rawCollection().findOne({username: username});
+    const post = await Posts.rawCollection().findOne({dummySlug: slug});
     const comment = {
       postId: post._id,
       userId: user._id,
@@ -67,14 +88,14 @@ if (getSetting('forum.seedOnStart')) {
       isDummy: true,
       disableNotifications: true,
     };
-    const parentComment = await Comments.rawCollection().findOne({ body: parentBody });
+    const parentComment = await Comments.rawCollection().findOne({body: parentBody});
 
     if (parentComment) {
       comment.parentCommentId = parentComment._id;
     }
 
     return newMutation({
-      collection: Comments, 
+      collection: Comments,
       document: comment,
       currentUser: user,
       validate: false,
@@ -89,7 +110,7 @@ if (getSetting('forum.seedOnStart')) {
     };
 
     return newMutation({
-      collection: Users, 
+      collection: Users,
       document,
       validate: false,
     });
@@ -105,16 +126,35 @@ if (getSetting('forum.seedOnStart')) {
     ]);
   };
 
+  const createDummyPics = async () => {
+    // eslint-disable-next-line no-console
+    console.log('// creating dummy pics…');
+    return Promise.all([
+      createPic('cherry_blossoms.jpg', moment().toDate(), `Kyoto's cherry blossoms`, 'Bruce'),
+      createPic('koyo.jpg', moment().subtract(10, 'minutes').toDate(), `Red maple leaves during Fall.`, 'Arnold'),
+      createPic('cat.jpg', moment().subtract(3, 'hours').toDate(), `This cat was staring at me for some reason…`,
+        'Julia'),
+      createPic('osaka_tram.jpg', moment().subtract(1, 'days').toDate(), `One of Osaka's remaining tram lines.`,
+        'Bruce', 'stackoverflow.png'),
+      createPic('matsuri.jpg', moment().subtract(2, 'days').toDate(), `A traditional Japanese dance.`, 'Julia'),
+      createPic('flowers.jpg', moment().subtract(6, 'days').toDate(), `Beautiful flowers!`, 'Julia'),
+      createPic('kyoto-night.jpg', moment().subtract(12, 'days').toDate(), `Kyoto at night`, 'Arnold'),
+      createPic('kaisendon.jpg', moment().subtract(20, 'days').toDate(), `This restaurant had the best kaisendon ever`,
+        'Julia'),
+      createPic('forest.jpg', moment().subtract(30, 'days').toDate(), `Such a peaceful place`, 'Bruce'),
+    ]);
+  };
+
   const createDummyPosts = async () => {
     // eslint-disable-next-line no-console
     console.log('// inserting dummy posts');
 
     return Promise.all([
-      createPost("read_this_first", moment().toDate(), "Bruce", "telescope.png"),
-      createPost("deploying", moment().subtract(10, 'minutes').toDate(), "Arnold"),
-      createPost("customizing", moment().subtract(3, 'hours').toDate(), "Julia"),
-      createPost("getting_help", moment().subtract(1, 'days').toDate(), "Bruce", "stackoverflow.png"),
-      createPost("removing_getting_started_posts", moment().subtract(2, 'days').toDate(), "Julia"),
+      createPost('read_this_first', moment().toDate(), 'Bruce', 'telescope.png'),
+      createPost('deploying', moment().subtract(10, 'minutes').toDate(), 'Arnold'),
+      createPost('customizing', moment().subtract(3, 'hours').toDate(), 'Julia'),
+      createPost('getting_help', moment().subtract(1, 'days').toDate(), 'Bruce', 'stackoverflow.png'),
+      createPost('removing_getting_started_posts', moment().subtract(2, 'days').toDate(), 'Julia'),
     ]);
   };
 
@@ -123,19 +163,20 @@ if (getSetting('forum.seedOnStart')) {
     console.log('// inserting dummy comments…');
 
     return Promise.all([
-      createComment("read_this_first", "Bruce", "What an awesome app!"),
-      createComment("deploying", "Arnold", "Deploy to da choppah!"),
-      createComment("deploying", "Julia", "Do you really need to say this all the time?", "Deploy to da choppah!"),
-      createComment("customizing", "Julia", "This is really cool!"),
-      createComment("removing_getting_started_posts", "Bruce", "Yippee ki-yay!"),
-      createComment("removing_getting_started_posts", "Arnold", "I'll be back.", "Yippee ki-yay!"),
+      createComment('read_this_first', 'Bruce', 'What an awesome app!'),
+      createComment('deploying', 'Arnold', 'Deploy to da choppah!'),
+      createComment('deploying', 'Julia', 'Do you really need to say this all the time?', 'Deploy to da choppah!'),
+      createComment('customizing', 'Julia', 'This is really cool!'),
+      createComment('removing_getting_started_posts', 'Bruce', 'Yippee ki-yay!'),
+      createComment('removing_getting_started_posts', 'Arnold', 'I\'ll be back.', 'Yippee ki-yay!'),
     ]);
   };
 
   Vulcan.removeGettingStartedContent = () => {
-    Users.remove({ 'profile.isDummy': true });
-    Posts.remove({ isDummy: true });
-    Comments.remove({ isDummy: true });
+    Users.remove({'profile.isDummy': true});
+    Posts.remove({isDummy: true});
+    Pics.remove({isDummy: true});
+    Comments.remove({isDummy: true});
     // eslint-disable-next-line no-console
     console.log('// Getting started content removed from seed_posts');
   };
@@ -143,8 +184,11 @@ if (getSetting('forum.seedOnStart')) {
   // Uses Promise.await to await async functions in a Fiber to make them "Meteor synchronous"
   Meteor.startup(() => {
     // insert dummy content only if createDummyContent hasn't happened and there aren't any posts or users in the db
-    if (!Users.find({ isDummy: true }).count()) {
+    if (!Users.find({isDummy: true}).count()) {
       Promise.await(createDummyUsers());
+    }
+    if (!Pics.find().count()) {
+      Promise.await(createDummyPics());
     }
     if (!Posts.find().count()) {
       Promise.await(createDummyPosts());
